7ba144fe33a37980e737505e752d0f71410481b6
danimjaffe <danieljaffe@mail.tau.ac.il>
2023-06-03 18:04:19 +0300

    added queue.c skeleton

---
+++ queue.c
@@ -0,0 +1,31 @@
+void initQueue(void) {
+    // Initialize the queue
+}
+
+void destroyQueue(void) {
+    // Destroy the queue and free resources
+}
+
+void enqueue(const void* data) {
+    // Add an item to the queue
+}
+
+void* dequeue(void)  {
+    // Remove and return an item from the queue
+}
+
+bool tryDequeue(void** data) {
+    // Try to remove and return an item from the queue, return true if successful
+}
+
+size_t size(void) {
+    // Get the current size of the queue
+}
+
+size_t size(void) {
+    // Get the current number of waiting threads
+}
+
+size_t visited(void) {
+    // Get the number of items that have passed inside the queue
+}
